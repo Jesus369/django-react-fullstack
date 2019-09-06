ff85712f0c5bd22c428604a15dd04047f049532d
Jesus <je.flores28@gmail.com>
2019-09-05 22:58:18 -0500

    DELETE, UPDATING ON DELETE, POST

--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,4 +1,4 @@
-import { GET_LEADS, DELETE_LEAD } from "../actions/types.js";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "../actions/types.js";
 
 const initialState = {
   leads: []
@@ -21,6 +21,12 @@ export default function(state = initialState, action) {
         // using the filter() and excluding the id
         ...state,
         leads: state.leads.filter(lead => lead.id !== action.payload)
+      };
+    case ADD_LEAD:
+      return {
+        // Returning all current leads plus the new lead(action.payload)
+        ...state,
+        leads: [...state.leads, action.payload]
       };
     default:
       return state;
